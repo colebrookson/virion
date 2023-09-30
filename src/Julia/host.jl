1b3ae35fa5e8f868c3b262bb32f7e7822228d87a
Cole Brookson <cole.brookson@gmail.com>
2023-09-29 23:19:21 -0400

    starting other form of organization

---
+++ src/Julia/host.jl
@@ -0,0 +1,9 @@
+using DataFrames
+using NCBITaxonomy
+import CSV
+
+include(joinpath(pwd(), "Code/Code_Dev/taxonomizer.jl"))
+
+names = DataFrame(CSV.File(joinpath(pwd(), "Code/Code_Dev/TaxonomyTempIn.csv"); delim=';'))
+reconciled_names = taxonomizer(names, :hosts; names=:Name)
+CSV.write(joinpath(pwd(), "Code/Code_Dev/TaxonomyTempOut.csv"), leftjoin(names, reconciled_names, on=:Name => :Original))
---
+++ src/R/functions_data_sourcing.R
@@ -0,0 +1,8 @@
+#' External data sourcing
+#' 
+#' There are a number of data streams that get pulled into this analysis. All
+#' the functions that do the web scraping for any data hosted outside of a 
+#' flat file happens here
+#' 
+#' 
+#' 
